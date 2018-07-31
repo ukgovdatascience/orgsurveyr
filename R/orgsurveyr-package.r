1e45b361df5ffbe77ca8d669b71bff63940f1ea9
Phil Chapman <phil.chapman@dwp.gsi.gov.uk>
2018-07-31 12:21:54 +0100

    sort out package doc page

--- R/orgsurveyr-package.r
+++ R/orgsurveyr-package.r
@@ -1,5 +1,15 @@
-#' orgsurveyr.
+#' @title Analyise and visualise organisation data
 #'
-#' @name orgsurveyr
+#' @description
+#' Lightweight wrapper for \pkg{ggraph} and \pkg{tidygraph} providing vignettes
+#' and convenience functions for the analysis of organisational data.
+#'
+#' @details
+#'
+#' To look at the package vignettes type \code{ browseVignettes('orgsurveyr')}
+#'
 #' @docType package
+#' @name orgsurveyr-package
+#' @aliases orgsurveyr
+#' @author Phil Chapman <phil.chapman@dwp.gsi.gov.uk>
 NULL
