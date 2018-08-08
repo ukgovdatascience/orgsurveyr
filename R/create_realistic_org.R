59b410f7b30c4288c5574aa0941188a9f6b923a3
Phil Chapman <phil.chapman@dwp.gsi.gov.uk>
2018-08-08 09:29:54 +0100

    update documentation for create_realistic_org

--- R/create_realistic_org.R
+++ R/create_realistic_org.R
@@ -4,27 +4,32 @@
 #' a maximum depth, but where units are then randomly removed according to probabilities set in either
 #' a single value for all units, a value per unit depth or a user defined function of depth.
 #'
-#' @param n_children integer
-#' @param max_depth integer
-#' @param prob double or vector of doubles
-#' @param .f function of x
-#' @param keep_creation_vars logical
+#' @param n_children number of child units per parent unit (integer)
+#' @param max_depth integer maximum depth of organisation (integer)
+#' @param prob  probability that a unit is deleted to create an irregular organisation.
+#' If a vector is provided that is the same length as max_depth then a different probability can be assigned to different
+#' levels of the organisation (double or vector of doubles)
+#' @param .f function of depth that generates probability that a unit is removed (NOT IMPLEMENTED) (function of x)
+#' @param delete_units retains intermediate variables and doesn't delete units - useful to understand how the function
+#' is working (logical)
 #'
 #' @return tidygraph object
 #' @export
 #' @importFrom magrittr %>%
 #'
 #' @examples
+#' set.seed(1234)
 #' tg1 <- create_realistic_org(4,3, prob=0.3)
 #' tg1
 #'
+#' set.seed(1234)
 #' tg2 <- create_realistic_org(4,3, prob=c(0.2, 0.4, 0.6))
 #' tg2
-create_realistic_org <- function(n_children = 4, max_depth = 3, prob=0.3, .f=NULL, keep_creation_vars=FALSE) {
+create_realistic_org <- function(n_children = 4, max_depth = 3, prob=0.3, .f=NULL, delete_units=TRUE) {
 
   stopifnot(length(prob) %in% c(1, max_depth))
   if(!is.null(.f)) stop('Use of function to define deletion probability not implemented yet')
-  stopifnot(is.logical(keep_creation_vars))
+  stopifnot(is.logical(delete_units))
 
   if (length(prob) == 1 & is.null(.f)) {
     prob_df <- tibble::data_frame(depth=0:max_depth, prob_deletion=prob)
@@ -42,11 +47,12 @@ create_realistic_org <- function(n_children = 4, max_depth = 3, prob=0.3, .f=NUL
     tidygraph::mutate(branch_delete = rbinom(nrow(tidygraph::.N()), 1, prob_deletion),
                       to_delete = tidygraph::map_bfs_dbl(1, .f = function(node, path, ...) {
                         max(tidygraph::.N()[c(node, path$node),]$branch_delete)
-                      })) #%>%
-  #tidygraph::filter(to_delete == 0)
+                      }))
 
-  if (!keep_creation_vars) {
-    out <- out %>% tidygraph::select(unit_id, depth)
+  if (delete_units) {
+    out <- out %>%
+      tidygraph::filter(to_delete == 0) %>%
+      tidygraph::select(unit_id, depth)
   }
 
   return(out)
