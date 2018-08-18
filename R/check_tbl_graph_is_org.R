3e9e65ff7f15acd0411cbfbecd637527c07bbfdf
Phil Chapman <phil.chapman@dwp.gsi.gov.uk>
2018-08-18 14:12:46 +0100

    add documentation for check_tbl_graph_is_org function

--- R/check_tbl_graph_is_org.R
+++ R/check_tbl_graph_is_org.R
@@ -1,3 +1,25 @@
+#' Check tbl_graph is an organisation
+#'
+#' Verify that a tbl_graph has the right features to be an organisation
+#'
+#' @param x
+#'
+#' @return logical
+#' @export
+#'
+#' @examples
+#'
+#' library(tidygraph)
+#'
+#' set.seed(1234)
+#' tg1 <- create_realistic_org()
+#' check_tbl_graph_is_org(tg1)
+#' # returns TRUE
+#'
+#' tg2 <- tidygraph::create_star(30)
+#' check_tbl_graph_is_org(tg2)
+#' # returns FALSE
+#'
 check_tbl_graph_is_org <- function(x) {
 
   if(!tidygraph::is.tbl_graph(x)) {
