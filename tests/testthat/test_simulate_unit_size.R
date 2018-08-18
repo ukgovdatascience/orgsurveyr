07d7bb6abd14b760b33c702b124b234d0cb4332d
Phil Chapman <phil.chapman@dwp.gsi.gov.uk>
2018-08-18 14:59:48 +0100

    add tests for simulate_unit_size output

---
+++ tests/testthat/test_simulate_unit_size.R
@@ -0,0 +1,33 @@
+context("Simulate unit size")
+
+library(dplyr)
+library(tidygraph)
+
+set.seed(1234)
+tg_ex1 <- create_realistic_org(n_children = 4, max_depth = 3, prob=0.3) %>%
+  simulate_unit_size()
+
+test_that("simulate_unit_size generates expected output", {
+
+  expect_is(tg_ex1, 'tbl_graph')
+
+  tg_ex1_nodes <- tg_ex1 %>% activate(nodes) %>% as_tibble()
+  tg_ex1_edges <- tg_ex1 %>% activate(edges) %>% as_tibble()
+
+  # number of edges and nodes
+  expect_equal(nrow(tg_ex1_edges), 41)
+  expect_equal(nrow(tg_ex1_nodes), 42)
+
+  # number of columns in nodes table
+  expect_equal(ncol(tg_ex1_nodes), 4)
+
+  # check values of unit size generated
+  expect_equal(tg_ex1_nodes %>% select(unit_size) %>% slice(1:5) %>% unlist() %>% unname(), c(3, 2, 1, 1, 3))
+
+})
+
+test_that("simulate_unit_size input parameter error handling works", {
+
+
+
+})
