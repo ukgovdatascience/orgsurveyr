6c5ae2d146e470891b18b1b848b2272ac1161cb0
Phil Chapman <phil.chapman@dwp.gsi.gov.uk>
2018-08-20 11:13:00 +0100

    add input parameter tests for generate_cumulative_mapping function

--- tests/testthat/test_generate_cumulative_mapping.R
+++ tests/testthat/test_generate_cumulative_mapping.R
@@ -1,6 +1,7 @@
 context("Generate cumulative mapping")
 
 library(dplyr)
+library(tidygraph)
 set.seed(1234)
 tg_ex1 <- create_realistic_org(n_children = 4, max_depth = 3, prob=0.3)
 tg_ex1 <- simulate_unit_size(tg_ex1)
@@ -20,4 +21,24 @@ test_that("generate_cumulative_mapping generates expected output", {
   expect_equal(group_size_df %>% select(n) %>% slice(1:5) %>% unlist() %>% unname(), c(146L, 59L, 64L, 20L, 21L))
 
 })
+
+test_that("generate_cumulative_mapping input parameter error handling works", {
+
+  # derived from check_tbl_graph_is_org
+  expect_error(generate_cumulative_mapping(1, df), "x is not a tbl_graph")
+  expect_error(generate_cumulative_mapping(create_star(20), df), "x is not a tree")
+  expect_warning(tg_ex1 %>% filter(unit_id != 1) %>% generate_cumulative_mapping(., df), 'x is not a rooted tree')
+
+  # check error message when df is not a data frame
+  expect_error(generate_cumulative_mapping(tg_ex1, 1), "inherits")
+
+  # check error message when unit_id not present
+  expect_error(generate_cumulative_mapping(tg_ex1, select(df, -unit_id)),
+               'df should have a unit_id and individual_id column')
+
+  expect_error(generate_cumulative_mapping(tg_ex1, select(df, -individual_id)),
+               'df should have a unit_id and individual_id column')
+
+})
+
 
