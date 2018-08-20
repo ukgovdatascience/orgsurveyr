49b1e980c0167db9b7f453189ead745cda544b8e
Phil Chapman <phil.chapman@dwp.gsi.gov.uk>
2018-08-20 09:56:23 +0100

    add documentation for generate_cumulative_mapping function

--- R/generate_cumulative_mapping_df.R
+++ R/generate_cumulative_mapping_df.R
@@ -1,3 +1,23 @@
+#' Generate Cumulative Mapping
+#'
+#' Takes an organisation as a tbl_graph object and a data frame that maps individuals to units
+#' and returns a data frame that maps an organisational unit to all individuals within that unit.
+#'
+#' For example, if an organisation is split into four departments, and each department has four teams,
+#' an employee is affiliated not just to their immediate team, but also their department and the organisation
+#' as a whole.
+#'
+#' The output dataframe serves as a basis for calculating unit level summaries that include all individuals
+#' affiliated to that unit.
+#'
+#' @param tg tbl_graph that passes a check with check_tbl_graph_is_org
+#' @param df a data frame with columns named unit_id and invididual_id
+#'
+#' @return tbl_df
+#' @export
+#'
+#' @examples
+#' NULL
 generate_cumulative_mapping <- function(tg, df) {
 
   check_tbl_graph_is_org(tg)
