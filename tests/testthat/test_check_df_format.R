35db3e338c587fae271faa8bdc0767c7b6fcb56c
Phil Chapman <phil.chapman@dwp.gsi.gov.uk>
2018-08-24 09:06:59 +0100

    add tests for output of check_df_format

--- tests/testthat/test_check_df_format.R
+++ tests/testthat/test_check_df_format.R
@@ -21,4 +21,34 @@ test_that("get_df_format output is correct", {
 
 test_that("check_df_format output is correct", {
 
+  expect_equal(check_df_format(tg_org_indiv_df, 'indiv_df', dev_mode=FALSE), TRUE)
+  expect_equal(check_df_format(tg_org_indiv_tall_df, 'indiv_tall_df', dev_mode=FALSE), TRUE)
+  expect_equal(check_df_format(tg_org_summarised_df, 'org_tall_df', dev_mode=FALSE), TRUE)
+  expect_equal(check_df_format(mtcars, 'unknown', dev_mode=FALSE), TRUE)
+
+  expect_equal(check_df_format(tg_org_indiv_df, 'unknown', dev_mode=FALSE), FALSE)
+  expect_equal(check_df_format(tg_org_indiv_tall_df, 'indiv_df', dev_mode=FALSE), FALSE)
+  expect_equal(check_df_format(tg_org_summarised_df, 'indiv_tall_df', dev_mode=FALSE), FALSE)
+  expect_equal(check_df_format(mtcars, 'org_tall_df', dev_mode=FALSE), FALSE)
+
+
+})
+
+test_that("check_df_format output is correct in dev_mode", {
+
+  expect_equal(check_df_format(tg_org_indiv_df, 'indiv_df', dev_mode=TRUE), TRUE)
+  expect_equal(check_df_format(tg_org_indiv_tall_df, 'indiv_tall_df', dev_mode=TRUE), TRUE)
+  expect_equal(check_df_format(tg_org_summarised_df, 'org_tall_df', dev_mode=TRUE), TRUE)
+  expect_equal(check_df_format(mtcars, 'unknown', dev_mode=TRUE), TRUE)
+
+  expect_error(check_df_format(tg_org_indiv_df, 'org_tall_df', dev_mode=TRUE),
+               "should be in org_tall_df format")
+  expect_error(check_df_format(tg_org_indiv_tall_df, 'indiv_df', dev_mode=TRUE),
+               "should be in indiv_df format")
+  expect_error(check_df_format(tg_org_summarised_df, 'indiv_tall_df', dev_mode=TRUE),
+               "should be in indiv_tall_df format")
+  expect_error(check_df_format(mtcars, 'org_tall_df', dev_mode=TRUE),
+               "should be in org_tall_df format")
+
+
 })
