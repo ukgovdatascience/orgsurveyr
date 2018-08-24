bb171fdb460d6ea301b2466a05a7ae3c008f3dd9
Phil Chapman <phil.chapman@dwp.gsi.gov.uk>
2018-08-24 17:31:36 +0100

    add skeleton documentation for example datasets

---
+++ R/orgsurveyr-data-examples.R
@@ -0,0 +1,35 @@
+#' @title Example organisation tbl_graph object
+
+#' @description
+#' TO ADD
+#'
+"tg_org"
+
+#' @title Example indiv_df data frame
+
+#' @description
+#' TO ADD
+#'
+"tg_org_indiv_df"
+
+#' @title Example minimal indiv_df data frame
+
+#' @description
+#' TO ADD
+#'
+"tg_org_indiv_minimal_df"
+
+#' @title Example indiv_tall_df data frame
+
+#' @description
+#' TO ADD
+#'
+"tg_org_indiv_tall_df"
+
+#' @title Example org_tall_df data frame
+
+#' @description
+#' TO ADD
+#'
+"tg_org_summarised_df"
+
