ce120c68ffaee95cfa57f663240173622959d583
Phil Chapman <phil.chapman@dwp.gsi.gov.uk>
2018-11-14 16:54:26 +0000

    v0.5.0: pass CRAN check

--- R/globals.R
+++ R/globals.R
@@ -1,5 +1,5 @@
 globalVariables(c(
   'child_id', 'org_depth', 'individual_id', 'individual_name', 'is_leaf', 'nodes',
   'prob_deletion', 'test_var', 'to_delete', 'unit_id', 'unit_size',
-  'parent_id', 'metric_id', 'value'
+  'parent_id', 'metric_id', 'value', '.'
 ))
